169c5b7f1e075be368bc0bac61cdde4dbf17356a
Kim <kwwendt@amazon.com>
2023-12-18 09:04:21 -0800

    Initial commit

---
+++ test/bucket-compliance.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const BucketCompliance = require('../lib/bucket-compliance-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bucket-compliance-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BucketCompliance.BucketComplianceStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
